28586bef9db8c354e282b4a2ffd103207abef9bf
TomA004 <tom.anderson@student.nua.ac.uk>
2023-05-17 16:54:17 +0100

    Finish

--- Source/MyProject2/MyProject2.Build.cs
+++ Source/MyProject2/MyProject2.Build.cs
@@ -9,7 +9,7 @@ public class MyProject2 : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-        PublicDependencyModuleNames.AddRange(new string[] { "MyProject2" });
+
         PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
